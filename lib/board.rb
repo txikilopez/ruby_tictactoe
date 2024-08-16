1135da09d3e03e29fddc351d86aeac4bc7efe577
Antonio <atroncoso10@gmail.com>
2024-08-16 11:11:16 -0400

    still thinking on solution

---
+++ lib/board.rb
@@ -0,0 +1,6 @@
+class TTTBoard
+  def initialize
+    @board = Array.new(3) {Array.new(3)}
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative lib/board
+require_relative lib/turn
+require 'pry-byebug'
+
+puts "Welcome to Tic Tac Toe please first player be x, second player will be o"
+
+board = Board.new()
+
+
